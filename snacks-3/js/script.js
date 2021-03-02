68898a1429f59975e09199eb9b14ed696e8d63ed
Tommaso <dossena.tommaso.dt@gmail.com>
2021-03-02 12:10:06 +0100

    Snack 3

---
+++ snacks-3/js/script.js
@@ -0,0 +1,19 @@
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri
+inseriti.
+*/
+
+// 1 - Inizializzare la variabile somma
+var numeri = [];
+var somma = 0;
+
+// 2 - Chiedere all'utente di inserire 10 numeri e li sommo
+for ( i = 0; i < 10; i++) {
+    numeri.push(parseInt(prompt("Inserisci un numero")));
+    somma += numeri[i];
+}
+
+// 3 - Stampare a schermo la somma dei numeri inseriti
+alert("La somma dei valori inseriti è " + somma);
+
