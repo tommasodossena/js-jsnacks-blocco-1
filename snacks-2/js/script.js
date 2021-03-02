f99b93eb674365bf8f3afde3160876ea4a2950e9
Tommaso <dossena.tommaso.dt@gmail.com>
2021-03-02 11:47:26 +0100

    Snack 2 - data validation

--- snacks-2/js/script.js
+++ snacks-2/js/script.js
@@ -1,7 +1,6 @@
 /*
-L’utente inserisce due parole in successione, con due
-prompt. Il software stampa prima la parola più corta, poi la
-parola più lunga.
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
 */
 
 // 1 - Chiedere all'utente di inserire la prima parola
@@ -10,11 +9,19 @@ var first = prompt("Inserisci la prima parola");
 // 2 - Chiedere all'utente di inserire la seconda parola
 var second = prompt("Inserisci la seconda parola");
 
-// 3 - Stampare a schermo la parola più corta seguita da quella più lunga
-if (first.length > second.length) {
-    alert (second + " " + first);
-} else if (second.length > first.length) {
-    alert (first + " " + second);
+// 3 - Validazione dati inseriti
+if (isNaN(first) && isNaN(second)) {
+
+    // 4 - Stampare a schermo la parola più corta seguita da quella più lunga
+    if (first.length > second.length) {
+        alert (second + " " + first);
+    } else if (second.length > first.length) {
+        alert (first + " " + second);
+    } else {
+        alert ("Le due parole hanno la medesima lunghezza");
+    }
+
 } else {
-    alert ("Le due parole hanno la medesima lunghezza");
+    alert ("Inserisci delle parole, i numeri non sono ammessi.");
+    location.reload();
 }
